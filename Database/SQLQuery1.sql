5e452e5214f11b8193747f49bba28b2c7ee2e700
Tony <tony.guirguis.dell@gmail.com>
2026-05-05 18:46:59 -0400

    restore sql cleanup and add balance columns

--- Database/SQLQuery1.sql
+++ Database/SQLQuery1.sql
@@ -1,6 +1,25 @@
 USE [SmartBankDB]
 GO
 
+Delete From LoginAttempts
+DBCC CHECKIDENT ('LoginAttempts', RESEED, 0);
+go
+Delete From AuditLog
+DBCC CHECKIDENT ('AuditLog', RESEED, 0);
+go
+Delete From Accounts
+DBCC CHECKIDENT ('Accounts', RESEED, 0);
+go
+Delete From Transactions
+DBCC CHECKIDENT ('Transactions', RESEED, 0);
+go
+Delete From Customers
+DBCC CHECKIDENT ('Customers', RESEED, 0);
+
+Delete From Users
+Where UserID <> 1
+GO
+
 ALTER TABLE Transactions ADD BalanceBefore DECIMAL(18,2) NULL;
 ALTER TABLE Transactions ADD BalanceAfter DECIMAL(18,2) NULL;
 GO
